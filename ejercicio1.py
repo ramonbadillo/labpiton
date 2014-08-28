ae2b281cdf6bfe3165166af174aeb641d8b29b3e
Luis Angel Sustaita <sustaita_7_7@hotmail.com>
2014-08-28 11:54:20 -0500

    hola

--- ejercicio1.py
+++ ejercicio1.py
@@ -61,6 +61,50 @@ def inter(set1, set2):
 	{3, 4}
 	"""
 	return {x for x in set1 for y in set2 if x==y}
+##Task 6.1
+##import math
+
+
+## Task 6.2
+##from random import radint
+
+## 6.3
+
+
+## 6.4
+
+def makeInverseIndex(strlist):
+    """
+    >>> makeInverseIndex()
+    Cosa
+    """
+    
+    f = open('stories_big.txt')
+    for line in f:
+        print(line)
+    
+    for x in [1,2,3]:
+        y = x*x
+        print(y)
+        
+
+
+
+
+def readFile():
+    """
+    >>> readFile()
+    True
+    """
+    
+    f = open('stories_big.txt')
+    ##for line in f:
+        ##print(line)
+    return f is not None
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 def prom(list):
 	"""
